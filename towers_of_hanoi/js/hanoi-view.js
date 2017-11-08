c5f96b90acd13f9afadd641a54f1e3d353610774
Brandt Sheets <brandtsheets@gmail.com>
2017-11-07 17:54:09 -0800

    First implementation of render

--- towers_of_hanoi/js/hanoi-view.js
+++ towers_of_hanoi/js/hanoi-view.js
@@ -2,23 +2,73 @@ class View {
   constructor(game, rootEl) {
     this.game = game;
     this.rootEl = rootEl;
+    this.secondClick = false;
+    this.source = -1;
     this.setupTowers();
+    this.bindEvents();
   }
 
   bindEvents() {
-
+    const $towers = $(".tower");
+    $towers.on("click", (event) => {
+      // console.log("click detected");
+      const $tower = $(event.currentTarget);
+      if (this.secondClick) {
+        $(".tower").removeClass("selected");
+        if (this.game.move(this.source, parseInt($tower.data("pos")))) {
+          //move successful
+          this.render();
+        }
+      } else {
+        this.source = parseInt($tower.data("pos"));
+        $tower.addClass("selected");
+      }
+      this.secondClick = !this.secondClick;
+    });
   }
 
   setupTowers() {
     for (let i = 0; i < 3; i++) {
-      this.rootEl.append($('<ul class="tower"></ul>'));
+      const $tower = $('<ul></ul>');
+      $tower.addClass("tower");
+      $tower.data("pos", i);
+      this.rootEl.append($tower);
     }
     for (let i = 0; i < 3; i++) {
       const $towers = $(".tower");
       const $disc = $("<li></li>");
       $disc.addClass("disc").addClass(`disc${i}`);
+      $disc.data("size", i);
       $($towers[0]).append($disc);
     }
+  }
+
+  render() {
+    $(".disc").remove();
+    const $towers = $(".tower");
+    for (let i = 0; i < 3; i++) {
+      const tower = this.game.towers[i];
+      for (let j = 0; j < tower.length; j++) {
+        const $disc = $("<li></li>");
+        const discSize = tower[j];
+        $disc.addClass("disc").addClass(`disc${j}`);
+        $($towers[j]).append($disc);
+      }
+    }
+  }
+
+  clickTower(event) {
+    console.log("click detected");
+    const $tower = $(event.currentTarget);
+    if (this.secondClick) {
+      this.game.move(this.source, parseInt($tower.data("pos")));
+      $(".tower").removeClass("selected");
+      this.render();
+    } else {
+      this.source = parseInt($tower.data("pos"));
+      $tower.addClass("selected");
+    }
+    this.secondClick = !this.secondClick;
   }
 }
 
